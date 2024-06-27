4849be3fb27788aef7f563a3f310429c7bb2c321
TheFive40 <jeanf2805@gmail.com>
2024-06-26 22:03:16 -0500

    Creacion del proyecto

---
+++ src/main/java/org/dbwztools/es/utilityplugin/UtilityPlugin.java
@@ -0,0 +1,16 @@
+package org.dbwztools.es.utilityplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UtilityPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
