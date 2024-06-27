47fc64a2a5d1620b564c258135ee0a3d1c985295
TheFive40 <jeanf2805@gmail.com>
2024-06-26 22:19:14 -0500

    Comando suicide

--- src/main/java/org/dbwztools/es/utilityplugin/UtilityPlugin.java
+++ src/main/java/org/dbwztools/es/utilityplugin/UtilityPlugin.java
@@ -1,12 +1,13 @@
 package org.dbwztools.es.utilityplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.dbwztools.es.utilityplugin.commands.CommandSuicide;
 
 public final class UtilityPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getCommand ( "suicide" ).setExecutor ( new CommandSuicide () );
     }
 
     @Override
---
+++ src/main/java/org/dbwztools/es/utilityplugin/commands/CommandSuicide.java
@@ -0,0 +1,19 @@
+package org.dbwztools.es.utilityplugin.commands;
+
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandSuicide implements CommandExecutor {
+    @Override
+    public boolean onCommand ( CommandSender commandSender, Command command, String s, String[] args ) {
+        if(commandSender instanceof Player player){
+            player.setHealthScale ( 0.0 );
+            player.sendMessage ( Color.RED + "Te has suicidado" );
+            return true;
+        }
+        return false;
+    }
+}
