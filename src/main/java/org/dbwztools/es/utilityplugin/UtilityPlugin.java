7101d60aef4ea4c1d1f200b2e41b99bed95a1153
Sebastián Pulido <splozano001@gmail.com>
2024-06-28 00:41:17 -0500

    Instanced commands and events

--- src/main/java/org/dbwztools/es/utilityplugin/UtilityPlugin.java
+++ src/main/java/org/dbwztools/es/utilityplugin/UtilityPlugin.java
@@ -2,7 +2,10 @@ package org.dbwztools.es.utilityplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dbwztools.es.utilityplugin.commands.CommandFalsificar;
+import org.dbwztools.es.utilityplugin.commands.CommandProhibir;
 import org.dbwztools.es.utilityplugin.commands.CommandSuicide;
+import org.dbwztools.es.utilityplugin.commands.CommandVanish;
+import org.dbwztools.es.utilityplugin.events.PickupEvent;
 
 public final class UtilityPlugin extends JavaPlugin {
     public static  UtilityPlugin plugin;
@@ -10,6 +13,9 @@ public final class UtilityPlugin extends JavaPlugin {
     public void onEnable() {
         getCommand ( "suicide" ).setExecutor ( new CommandSuicide () );
         getCommand("falsificar").setExecutor(new CommandFalsificar());
+        getCommand("invisible").setExecutor(new CommandVanish());
+        getCommand("prohibir").setExecutor(new CommandProhibir());
+        getServer().getPluginManager().registerEvents(new PickupEvent(),this);
         plugin = this;
     }
 
